619827e21746b66a786ec310744d9c5806b4be8d
khusnul khotimah <khusnulkhotimahuut@gmail.com>
2023-11-15 13:50:24 +0700

    edit dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -13,7 +13,7 @@
             <div class="col mr-2">
               <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
                 Laporan Penjualan</div>
-              <div class="h5 mb-0 font-weight-bold text-gray-800">Rp.40.000</div>
+              <div class="h5 mb-0 font-weight-bold text-gray-800">0</div>
             </div>
             <div class="col-auto">
               <i class="fas fa-calendar fa-2x text-gray-300"></i>
