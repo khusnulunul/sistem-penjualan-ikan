ea72dc0eff5859c15cd9b2e24a0ebabb7aa3b56c
raudatul18 <raudatulmusyrofahmaulaisjati@gmail.com>
2023-11-15 13:58:59 +0700

    m

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -37,7 +37,7 @@
               <div class="col-lg-6">
                 <div class="p-5">
                   <div class="text-center">
-                    <h1 class="h4 text-gray-900 mb-4">From login</h1>
+                    <h1 class="h4 text-gray-900 mb-4">Form login</h1>
                   </div>
                   <form action="{{ route('login.aksi') }}" method="POST" class="user">
                     @csrf
