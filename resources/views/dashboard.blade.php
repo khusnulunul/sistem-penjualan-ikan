f6a9a37ba3cd5eccb5ef4db1fc570d4f9be45349
suciamalia15 <suciamalia394@gmail.com>
2023-11-15 14:01:41 +0700

    Edit dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -47,11 +47,11 @@
         <div class="card-body">
           <div class="row no-gutters align-items-center">
             <div class="col mr-2">
-              <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Tasks
+              <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Customer
               </div>
               <div class="row no-gutters align-items-center">
                 <div class="col-auto">
-                  <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">50%</div>
+                  <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">0</div>
                 </div>
                 <div class="col">
                   <div class="progress progress-sm mr-2">
